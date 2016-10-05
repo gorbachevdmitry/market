43c5facc30d8c54e4209bb01f85e651b5de9e822
gorbachevdmitry <dmgorbacheww@gmail.com>
2016-10-05 07:16:21 +0300

    add deploy settings

---
+++ magazin/magazin/deploy_settings/__init__.py
@@ -0,0 +1,14 @@
+
+from ..settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '.pythonanywhere.com',
+]
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
